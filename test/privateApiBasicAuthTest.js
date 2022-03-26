7d6f094f75891236c30b8a78eb173255622feddc
agga1211 <54178133+agga1211@users.noreply.github.com>
2022-03-27 00:29:40 +0530

    .set method replaced by .auth

--- test/privateApiBasicAuthTest.js
+++ test/privateApiBasicAuthTest.js
@@ -5,25 +5,15 @@ const server = require('../index');
 
 chai.use(chaiHttp);
 
-var username = "ayush";
-var password = "123456";
-
-function authenticateUser(user, password)
-{
-    var token = user + ":" + password;
-
-    // Base64 Encoding -> btoa
-    var hash = btoa(token); 
-
-    return "Basic " + hash;
-}
+const username = "ayush";
+const password = "123456";
 
 describe('Private API', () => {
 
     it('Private API should return success as it is authorized.', (done) => {
         chai.request(server)
         .get('/')
-        .set('Authorization', authenticateUser(username, password))
+        .auth(username, password)
         .end((err, response) => {
             expect(response.status).to.be.equal(200);
             expect(response.body).to.have.all.keys('status', 'message');
